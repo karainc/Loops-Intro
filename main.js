c32a1636b85a47d750e1b3a8351a6d5e11f9e53e
Kara Hall <karalynn.inc@gmail.com>
2023-02-14 15:56:53 -0500

    Loop exercises completed because I am a rockstar

---
+++ main.js
@@ -0,0 +1,14 @@
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+for (var i =0; i < cohortOneStudents.length; i++) {
+    console.log(cohortOneStudents[i]);
+}
+
+var myInterests = ["Reading", "Yoga", "Cooking", "4-Wheeling", "Music"];
+var i = 0;
+
+for(var i = 0; i < myInterests.length; i++)
+if (i === 1) {
+    console.log(`My favorite interest is ${myInterests[1]}`)
+} else {
+console.log('One of my interests is ' + myInterests[i]);
+}
